b99b25ff9ee8ea1693197cd0de6d611646af6d62
Kelvin Kosgei <BrevinKelvin@kelvins-macbook-air.local>
2016-06-17 20:10:25 +0300

    Finished Part 9

--- MusicVideo/MusicVideo/ViewController.swift
+++ MusicVideo/MusicVideo/ViewController.swift
@@ -8,8 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    var videos = [Videos]()
 
+    @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var displayLabel: UILabel!
     
     override func viewDidLoad() {
@@ -22,13 +25,15 @@ class ViewController: UIViewController {
         
 //        Call API
         let api = APIManager()
-        api.loadData("https://itunes.apple.com/us/rss/topmusicvideos/limit=10/json", completion: didLoadData)
+        api.loadData("https://itunes.apple.com/us/rss/topmusicvideos/limit=50/json", completion: didLoadData)
      
     }
     
     func didLoadData(videos: [Videos]) {
         
         print(reachabilityStatus)
+        
+        self.videos = videos
 
         for item in videos{
             print("name = \(item.vName)")
@@ -37,6 +42,8 @@ class ViewController: UIViewController {
         for (index,item) in videos.enumerate() {
             print("\(index) artist = \(item.vArtist)")
         }
+        
+        tableView.reloadData()
         
     }
     
@@ -58,6 +65,42 @@ class ViewController: UIViewController {
     {
         NSNotificationCenter.defaultCenter().removeObserver(self, name: "ReachStatusChanged",object: nil)
     }
+    
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    
+    {
+        
+        return videos.count
+        
+        
+        
+    }
+    
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int
+    
+    
+    {
+        return 1
+        
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
+    {
+        let cell = tableView.dequeueReusableCellWithIdentifier("cell",forIndexPath: indexPath)
+        
+        let video = videos[indexPath.row]
+        
+        cell.textLabel?.text = ("\(indexPath.row + 1)")
+        cell.detailTextLabel?.text = video.vName
+        
+        return  cell
+    }
+    
+    
+    
+
 
     
 
